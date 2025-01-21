bd8e8cad15376ca4451727c79269d7beff439a2c
zheen <suseyizheen@gmail.com>
2025-01-21 14:03:17 -0800

    added an aspectvgrid

---
+++ Memorize/AspectVGrid.swift
@@ -0,0 +1,18 @@
+//
+//  AspectVGrid.swift
+//  Memorize
+//
+//  Created by Zheen Suseyi on 1/20/25.
+//
+
+import SwiftUI
+
+struct AspectVGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AspectVGrid()
+}
